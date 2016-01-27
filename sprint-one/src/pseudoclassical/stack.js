96bc6f6b8341c2119c83fe135e2c8ac3720fa677
Dejon Gill <dejonjgill@gmail.com>
2016-01-27 14:24:31 -0800

    Psuedoclassical Stack Complete

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -8,6 +8,24 @@ var Stack = function() {
 
 };
 
+Stack.prototype.push = function(value) {
+
+  this.storage[this.count] = value;
+
+  this.count++;
+
+};
+
+Stack.prototype.pop = function() {
+
+  if (this.count) {
+    this.count--;
+  }
+
+  return this.storage[this.count];
+
+};
+
 Stack.prototype.size = function() {
 
   return this.count;
