f1acd792bbc7db8a035edaa56404bedd13042d66
kathybeatz <kathleeng.lopez@gmail.com>
2016-01-27 11:58:01 -0800

    Prototypal stack.js complete!

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,41 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = Object.create(stackMethods); 
+
+  someInstance.count = 0;
+
+  someInstance.storage = {};
+
+  return someInstance;
+
 };
 
-var stackMethods = {};
+var stackMethods = {}; //We believe t-h-i-s is unnecessary
 
 
+stackMethods.push = function(value) {
+
+  this.storage[this.count] = value;
+
+  this.count++;
+
+};
+
+stackMethods.pop = function() {
+
+  if (this.count) {
+    this.count--;
+  }
+
+  return this.storage[this.count];
+
+};
+
+
+stackMethods.size = function() {
+
+  return this.count;
+
+};
