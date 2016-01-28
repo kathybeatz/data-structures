35484b329f4c7e925026c589da19635c6c2315a8
kathybeatz <kathleeng.lopez@gmail.com>
2016-01-28 12:31:28 -0800

    Began linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,51 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    this.tail = new Node(value);
+    // console.log('tail', this.tail.value);
+    // if no values in list yet
+    if(list.head === null){
+      // this.head.value = value;
+      // this.tail.value = value;
+      // this.head.next = value;
+      // alert('yatta');
+      this.head = this.tail;
+    }
+    //if 1+ value already exists in the list
+    else {
+
+      this.head.value = this.tail.value;
+      this.tail.value = value;
+      this.head.next = this.tail.value;
+
+    }
+
+
   };
 
   list.removeHead = function(){
+    console.log('head.val at begging', this.head);
+    var temp = this.head.value;
+    delete this.head;
+    console.log('head.val after deleted', this.head);
+    this.head = this.tail;
+    // console.log('this remove head', this);
+    console.log('this.head after assigning tail to head', this.head);
+    console.log('this.tail', this.tail);
+     
+
+    // return this.value;
+    return temp;
+
   };
 
   list.contains = function(target){
+    // for (var key in list) {
+      if (target in list) {
+        return true;
+      }
+    // }
+    return false;
   };
 
   return list;
